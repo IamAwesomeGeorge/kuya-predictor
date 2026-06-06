3f6d45b576c5a38c5a10ffd3ece7e0f99f20d6c0
IamAwesomeGeorge <83273399+IamAwesomeGeorge@users.noreply.github.com>
2026-06-06 15:30:50 +0100

    predict card

--- src/components/DevelopmentNotice.tsx
+++ src/components/DevelopmentNotice.tsx
@@ -2,11 +2,15 @@ import { Alert, AlertTitle, Box } from "@mui/material";
 
 export default function DevelopmentNotice() {
   return (
-    <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-      <Alert severity="warning" variant="filled">
-        <AlertTitle>Still Under Development</AlertTitle>
-        Information displayed here may be inaccurate.
-      </Alert>
-    </Box>
+    <>
+      {import.meta.env.VITE_HIDE_WARNING !== "true" && (
+        <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <Alert severity="warning" variant="filled">
+            <AlertTitle>Still Under Development</AlertTitle>
+            Information displayed here may be inaccurate.
+          </Alert>
+        </Box>
+      )}
+    </>
   );
 }
--- src/pages/Predict/Predict.tsx
+++ src/pages/Predict/Predict.tsx
@@ -1,13 +1,20 @@
-import DevelopmentNotice from "../../components/DevelopmentNotice";
+import { Grid } from "@mui/material";
 import PageHeader from "../../components/header/PageHeader";
+import PredictSkeletonCard from "../../components/predict/card/PredictSkeletonCard";
 
 export default function Predict() {
   return (
     <>
       <PageHeader title="Predict" />
-      <DevelopmentNotice />
       <h1>Predictions not open yet</h1>
       <p>Still in development. Inputs likely to be removed.</p>
+
+      <Grid container spacing={1}>
+        <PredictSkeletonCard />
+        <PredictSkeletonCard />
+        <PredictSkeletonCard />
+        <PredictSkeletonCard />
+      </Grid>
     </>
   );
 }
