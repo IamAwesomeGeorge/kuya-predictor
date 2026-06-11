2a456ea5dfb559db7716639b86040beceb65baa6
IamAwesomeGeorge <83273399+IamAwesomeGeorge@users.noreply.github.com>
2026-06-11 13:12:52 +0100

    Revert "better laoding"
    This reverts commit 461ea32949412b0bc348bb53c355ed99f3741090.

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,14 @@
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Suspense, StrictMode, lazy } from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./routes/__root";
 import { UserProvider } from "./contexts/UserProvider";
 import { TeamsProvider } from "./contexts/TeamsProvider";
-
-const ToastContainer = lazy(() =>
-  import("react-toastify").then((module) => ({
-    default: module.ToastContainer,
-  })),
-);
-
-const Analytics = lazy(() =>
-  import("@vercel/analytics/react").then((module) => ({
-    default: module.Analytics,
-  })),
-);
-
-const SpeedInsights = lazy(() =>
-  import("@vercel/speed-insights/react").then((module) => ({
-    default: module.SpeedInsights,
-  })),
-);
+import { ToastContainer } from "react-toastify";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const queryClient = new QueryClient();
 
@@ -37,11 +22,10 @@ if (!rootElement.innerHTML) {
         <UserProvider>
           <TeamsProvider>
             <RouterProvider router={router} />
-            <Suspense fallback={null}>
-              <ToastContainer />
-              <Analytics />
-              <SpeedInsights />
-            </Suspense>
+            <ToastContainer />
+            {/* Analytics */}
+            <Analytics />
+            <SpeedInsights />
           </TeamsProvider>
         </UserProvider>
       </QueryClientProvider>
