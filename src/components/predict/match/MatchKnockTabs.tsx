35ac24194eb1eb1cd4a5c76b00edda1dcb47729c
IamAwesomeGeorge <83273399+IamAwesomeGeorge@users.noreply.github.com>
2026-06-26 15:37:08 +0100

    !!!

--- src/components/predict/match/MatchKnockTabs.tsx
+++ src/components/predict/match/MatchKnockTabs.tsx
@@ -8,6 +8,7 @@ import MatchBase from "./MatchBase";
 import { UserContext } from "../../../contexts/UserContext";
 import type { PredictMatchView } from "../../../models/Predict";
 import { isMobile } from "../../utils/MobileUtils";
+import PriorityHighIcon from "@mui/icons-material/PriorityHigh";
 
 export default function MatchKnockoutTabs({ preview = false }: { preview?: boolean }) {
   const [mode, setMode] = useState(0);
@@ -49,12 +50,19 @@ export default function MatchKnockoutTabs({ preview = false }: { preview?: boole
     4: "FINAL",
   };
 
+  const isComplete = (stage: Stage) => {
+    const matches = findMatchesFromStage(stage);
+    const matchesFiltered = matches?.filter((m) => m.team_left !== "ZZ" && m.team_right !== "ZZ");
+    const predictions = findPredictionsFromStage(stage);
+    return matchesFiltered?.length === predictions?.length;
+  };
+
   const stageComplete = {
-    ROUND_OF_32: findMatchesFromStage("ROUND_OF_32")?.length === findPredictionsFromStage("ROUND_OF_32")?.length,
-    ROUND_OF_16: findMatchesFromStage("ROUND_OF_16")?.length === findPredictionsFromStage("ROUND_OF_16")?.length,
-    QUARTERFINAL: findMatchesFromStage("QUARTERFINAL")?.length === findPredictionsFromStage("QUARTERFINAL")?.length,
-    SEMIFINAL: findMatchesFromStage("SEMIFINAL")?.length === findPredictionsFromStage("SEMIFINAL")?.length,
-    FINAL: findMatchesFromStage("FINAL")?.length === findPredictionsFromStage("FINAL")?.length,
+    ROUND_OF_32: isComplete("ROUND_OF_32"),
+    ROUND_OF_16: isComplete("ROUND_OF_16"),
+    QUARTERFINAL: isComplete("QUARTERFINAL"),
+    SEMIFINAL: isComplete("SEMIFINAL"),
+    FINAL: isComplete("FINAL"),
   };
 
   return (
@@ -69,21 +77,34 @@ export default function MatchKnockoutTabs({ preview = false }: { preview?: boole
           >
             <Tab
               label={isMobile() ? "R32" : "Round of 32"}
+              icon={stageComplete["ROUND_OF_32"] ? undefined : <PriorityHighIcon fontSize="small" />}
+              iconPosition="end"
               sx={{ color: stageComplete["ROUND_OF_32"] ? "#c8ffc8" : "#ffc8c8" }}
             />
             <Tab
               label={isMobile() ? "R16" : "Round of 16"}
+              icon={stageComplete["ROUND_OF_16"] ? undefined : <PriorityHighIcon fontSize="small" />}
+              iconPosition="end"
               sx={{ color: stageComplete["ROUND_OF_16"] ? "#c8ffc8" : "#ffc8c8" }}
             />
             <Tab
               label={isMobile() ? "QF" : "Quarterfinals"}
+              icon={stageComplete["QUARTERFINAL"] ? undefined : <PriorityHighIcon fontSize="small" />}
+              iconPosition="end"
               sx={{ color: stageComplete["QUARTERFINAL"] ? "#c8ffc8" : "#ffc8c8" }}
             />
             <Tab
               label={isMobile() ? "SF" : "Semifinals"}
+              icon={stageComplete["SEMIFINAL"] ? undefined : <PriorityHighIcon fontSize="small" />}
+              iconPosition="end"
               sx={{ color: stageComplete["SEMIFINAL"] ? "#c8ffc8" : "#ffc8c8" }}
             />
-            <Tab label={isMobile() ? "F" : "Final"} sx={{ color: stageComplete["FINAL"] ? "#c8ffc8" : "#ffc8c8" }} />
+            <Tab
+              label={isMobile() ? "F" : "Final"}
+              icon={stageComplete["FINAL"] ? undefined : <PriorityHighIcon fontSize="small" />}
+              iconPosition="end"
+              sx={{ color: stageComplete["FINAL"] ? "#c8ffc8" : "#ffc8c8" }}
+            />
           </Tabs>
 
           {Object.entries(groupsIndexMap).map(([index, stage]) => (
